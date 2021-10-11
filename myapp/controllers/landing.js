db5444f32812f2a1b12cc99f37be95adaf5d1c05
Steve Larsen <slarsen-3@att.net>
2021-10-11 12:20:57 -0500

    Implement /leads route & show a collection of leads on submission

--- myapp/controllers/landing.js
+++ myapp/controllers/landing.js
@@ -1,9 +1,20 @@
+const models = require('../models');
 
 exports.get_landing = function (req, res, next) {
   res.render('landing', { title: 'Express' });
 };
 exports.submit_lead = function (req, res, next) {
   // res.render('landing', { title: 'Express' });
-  console.log("lead email:", req.body.lead_email)
-  res.redirect('/');
+  // console.log("lead email:", req.body.lead_email)
+  return models.Lead.create({
+    email: req.body.lead_email,
+  }).then((lead) => {
+    res.redirect('/leads');
+  });
+};
+
+exports.show_leads = function (req, res, next) {
+  models.Lead.findAll().then(leads => {
+    res.render('landing', {title: 'Express', leads: leads});
+  })
 };
